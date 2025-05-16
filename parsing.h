e963be37775511805328be1852bea466e87440a1
Purpose <riyane.dev@gmail.com>
2025-05-16 10:47:34 +0200

    feat(parsing): add parsing.h header file
    ---
    Introduce `parsing.h` to declare input validation and parsing functions for the push_swap project. Includes `check_arguments` and `parse_input` function prototypes.

---
+++ parsing.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 10:46:16 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/05/16 10:47:20 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_H
+# define PARSING_H
+
+# include "push_swap.h"
+
+int		check_arguments(int argc, char **argv);
+t_stack	*parse_input(int argc, char **argv);
+
+#endif // PARSING_H
