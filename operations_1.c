2e169b8e2e256738005b2772f4330e55ad3032dd
Purpose <riyane.dev@gmail.com>
2025-05-16 11:49:11 +0200

    feat(stack_operations): add basic stack manipulation functions
    ---
    Added operations `sa`, `sb`, `ss`, `pa`, and `pb` to perform basic stack manipulations such as swapping and pushing elements between stacks.

---
+++ operations_1.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations_1.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 10:52:37 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/05/16 11:48:36 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sorting.h"
+
+void	sa(t_stack *a)
+{
+	int	tmp;
+
+	if (a->size < 2)
+		return ;
+	tmp = a->top->value;
+	a->top->value = a->top->next->value;
+	a->top->next->value = tmp;
+}
+
+void sb(t_stack *b)
+{
+	sa(b);
+}
+
+void	ss(t_stack *a, t_stack *b)
+{
+	sa(a);
+	sb(b);
+}
+
+void pa(t_stack *a, t_stack *b)
+{
+	if (b->size > 0)
+		push(a, pop(b));
+}
+
+void pb(t_stack *a, t_stack *b)
+{
+	pa(b, a);
+}
