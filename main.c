c58fe7a01195ecb1a2b48b95348179f80f2bbad6
Purpose <riyane.dev@gmail.com>
2025-05-16 12:25:47 +0200

    feat(main): add initial implementation of sorting algorithm driver
    ---
    Add `main.c` as the entry point for the sorting application. This file includes stack initialization, argument validation, sorting algorithm selection based on input size, and memory cleanup functionality.

---
+++ main.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 11:51:46 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/05/16 12:24:23 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "parsing.h"
+#include "sorting.h"
+
+static void	initialize_stacks(int argc, char **argv, t_stack **a, t_stack **b)
+{
+	int	valid_args;
+
+	valid_args = check_arguments(argc, argv);
+	if (!valid_args)
+		error_and_exit("Error");
+	*a = parse_input(argc, argv);
+	*b = init_stack();
+}
+
+static void	choose_sorting(t_stack *a, t_stack *b)
+{
+	int	size;
+
+	size = a->size;
+	if (size <= 1)
+		return ;
+	if (size == 2)
+		sort_two(a);
+	else if (size == 3)
+		sort_three(a);
+	else if (size <= 5)
+		sort_five(a, b);
+	else
+		sort_large(a, b, size);
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack	*a;
+	t_stack	*b;
+
+	if (argc < 2)
+		return (0);
+	initialize_stacks(argc, argv, &a, &b);
+	choose_sorting(a, b);
+	clear_stack(a);
+	clear_stack(b);
+	return (0);
+}
