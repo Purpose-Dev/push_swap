a819e0238ab1377b547b780801f01c8e876925e5
Purpose <riyane.dev@gmail.com>
2025-05-16 11:41:20 +0200

    feat(parser): add input parsing with validation for numbers and duplicates
    ---
    Added `parse_input` function to handle command-line arguments, validate them as integers, check for duplicates, and construct a stack.

---
+++ parse.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 10:51:44 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/05/16 11:40:46 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+static int	is_number(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (FALSE);
+		i++;
+	}
+	retur (TRUE);
+}
+
+static int	has_duplicate(int argc, char **argv)
+{
+	t_node	*current;
+
+	current = stack->top;
+	while (current)
+	{
+		if (current->value == value)
+			return (TRUE);
+		current = current->next;
+	}
+	return (FALSE);
+}
+
+int check_arguments(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (!is_number(argv[i]))
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
+t_stack *parse_input(int argc, char **argv)
+{
+	int		i;
+	t_stack	*a;
+
+	i = 1;
+	a = init_stack();
+	while (i < argc)
+	{
+		int value = ft_atoi(argv[i]);
+		if (has_duplicate(a, value))
+			error_and_exit("Error: Duplicate values");
+		push(a, value);
+		i++;
+	}
+	return (a);
+}
