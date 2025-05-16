1672c00c097de7ae0118b5fbdc25d1f34d100515
Purpose <riyane.dev@gmail.com>
2025-05-16 10:49:25 +0200

    feat(sorting): add sorting header file
    ---
    Introduced `sorting.h` defining stack manipulation functions for sorting operations, including swaps, pushes, and rotations. These functions will support the core logic of the `push_swap` project.

---
+++ sorting.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sorting.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 10:47:54 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/05/16 10:49:02 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SORTING_H
+# define SORTING_H
+
+# include "push_swap.h"
+
+void	sa(t_stack *a);
+void	sb(t_stack *b);
+void	ss(t_stack *a, t_stack *b);
+void	pa(t_stack *a, t_stack *b);
+void	pb(t_stack *a, t_stack *b);
+void	ra(t_stack *a);
+void	rb(t_stack *b);
+void	rr(t_stack *a, t_stack *b);
+void	rra(t_stack *a);
+void	rrb(t_stack *b);
+void	rrr(t_stack *a, t_stack *b);
+
+#endif // SORTING_H
