4179b23fde5ddd17ce365a7cebf193306187c3a5
Purpose <riyane.dev@gmail.com>
2025-05-16 18:04:58 +0200

    feat(parsing): enhance argument validation and add '-v' flag support
    ---
    Improved `check_arguments` to include duplicate detection, integer range validation, and an optional `-v` flag for verbose mode. Added a helper function `is_valid_int` for integer range checks and updated parsing logic accordingly.

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 10:51:44 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/05/16 12:26:51 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/05/16 18:04:25 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static int	is_number(const char *str)
 		i++;
 	while (str[i])
 	{
-		if (str[i] < '0' || str[i] > '9')
+		if (!ft_isdigit(str[i]))
 			return (FALSE);
 		i++;
 	}
@@ -42,15 +42,38 @@ static int	has_duplicate(t_stack *stack, int value)
 	return (FALSE);
 }
 
-int	check_arguments(int argc, char **argv)
+static int	is_valid_int(const char *str)
+{
+	int	val;
+
+	val = ft_atoi(str);
+	if (val < INT_MIN || val > INT_MAX)
+		return (FALSE);
+	return (TRUE);
+}
+
+int	check_arguments(int argc, char **argv, int *v_flag)
 {
 	int	i;
+	int	j;
 
-	i = 1;
+	*v_flag = 0;
+	if (argc <= 1)
+		return (FALSE);
+	if (!ft_strcmp(argv[1], "-v"))
+		*v_flag = 1;
+	i = 1 + *v_flag;
 	while (i < argc)
 	{
-		if (!is_number(argv[i]))
+		if (!is_number(argv[i]) || !is_valid_int(argv[i]))
 			return (FALSE);
+		j = i + 1;
+		while (j < argc)
+		{
+			if (!ft_strcmp(argv[i], argv[j]))
+				return (FALSE);
+			j++;
+		}
 		i++;
 	}
 	return (TRUE);
--- parsing.h
+++ parsing.h
@@ -6,7 +6,7 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 10:46:16 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/05/16 10:47:20 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/05/16 18:04:25 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "push_swap.h"
 
-int		check_arguments(int argc, char **argv);
+int		check_arguments(int argc, char **argv, int *v_flag);
 t_stack	*parse_input(int argc, char **argv);
 
 #endif // PARSING_H
