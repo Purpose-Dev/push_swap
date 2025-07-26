a96f942060e63b95a016f836547085e65851810b
Riyane El Qoqui <riyane.dev@gmail.com>
2025-07-26 11:30:32 +0200

    chore(headers): add checker.h

--- includes/checker.h
+++ includes/checker.h
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   checker.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/13 00:00:02 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/05/13 00:00:16 by rel-qoqu         ###   ########.fr       */
+/*   Created: 2025/07/26 11:25:51 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/07/26 11:28:47 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef CHECKER_H
+# define CHECKER_H
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
-{
-	t_list	*last;
+# include "push_swap.h"
 
-	if (!lst || !new)
-		return ;
-	if (*lst == NULL)
-	{
-		*lst = new;
-		return ;
-	}
-	last = ft_lstlast(*lst);
-	last->next = new;
-}
+int		handle_error_and_cleanup(t_stack *stack_a, t_stack *stack_b);
+void	init_and_parse(const int argc, char **argv, t_stack **stack_a,
+			t_stack *stack_b);
+
+#endif // CHECKER_H
