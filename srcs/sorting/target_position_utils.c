1a06968b4e18453bfb76b9c411faae4362dc50f0
Riyane El Qoqui <riyane.dev@gmail.com>
2025-07-26 13:19:02 +0200

    fix(sorting): optimize target position lookup by refining greater value logic

--- srcs/sorting/target_position_utils.c
+++ srcs/sorting/target_position_utils.c
@@ -6,7 +6,7 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 11:45:34 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/07/26 11:49:22 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/07/26 13:17:23 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@ static int	find_greater_target(t_stack *a, const int value)
 	t_dlist_node	*current;
 	int				pos;
 	int				target_pos;
+	int				min_greater_value;
 	t_element		*elem;
-	t_element		*target_elem;
 
 	current = a->head;
+	target_pos = -1;
+	min_greater_value = INT_MAX;
 	pos = 0;
-	target_pos = 0;
 	while (current)
 	{
-		elem = (t_element *) current->data;
-		if (elem->value > value)
+		elem = (t_element *)current->data;
+		if (elem->value > value && elem->value < min_greater_value)
 		{
-			target_elem = (t_element *) dlist_get_at(a, target_pos);
-			if (target_pos == 0 || elem->value < target_elem->value)
-				target_pos = pos;
+			min_greater_value = elem->value;
+			target_pos = pos;
 		}
 		current = current->next;
 		pos++;
@@ -67,11 +67,9 @@ static int	find_min_value_target(const t_stack *a)
 int	find_target_position(t_stack *a, const int value)
 {
 	int			target_pos_greater;
-	t_element	*target_elem;
 
 	target_pos_greater = find_greater_target(a, value);
-	target_elem = (t_element *) dlist_get_at(a, target_pos_greater);
-	if (target_elem && target_elem->value > value)
+	if (target_pos_greater != -1)
 		return (target_pos_greater);
 	return (find_min_value_target(a));
 }
