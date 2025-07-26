c9b6975ebeafa1713483177147f09b789ca877de
Riyane El Qoqui <riyane.dev@gmail.com>
2025-07-26 12:36:45 +0200

    chore(comparator): move comparator.c to srcs directory and update include path

--- srcs/comparator.c
+++ srcs/comparator.c
@@ -6,17 +6,17 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 10:51:36 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/05/16 10:59:16 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/07/26 12:36:38 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "comparator.h"
+#include "../includes/comparator.h"
 
-int	ft_compare(int a, int b)
+int	ft_compare(const int a, const int b)
 {
 	if (a > b)
 		return (1);
-	else if (a < b)
+	if (a < b)
 		return (-1);
 	return (0);
 }
